e936cabc20640279efb24ac3e2dd6b4eaf0756c4
ljg02 <gyu021211@gmail.com>
2025-01-23 21:03:24 +0900

    final

--- My project/Assets/Scripts/OrbData.cs
+++ My project/Assets/Scripts/OrbData.cs
@@ -9,5 +9,6 @@ public class OrbData
     public string orbText; //텍스트 연동
     public Sprite orbImage; // 이미지 연동 시 사용
     public VideoClip orbVideo; // 영상 연동 시 사용
+    public Color orbColor; // 색상 연동 시 사용
     // 추가적인 데이터 필드 추가 가능
 }
